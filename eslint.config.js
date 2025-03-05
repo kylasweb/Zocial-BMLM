d647fa1db47f9160687c0a83341343d3a7d6ba87
Padmasree <leonedarko@loves.software>
2025-03-05 20:12:53 +0530

    Remove unused package.json and type definition files from various dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,31 +3,71 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default {
-  ignores: ['dist', 'node_modules'],
-  extends: [js.configs.recommended],
-  files: ['**/*.{js,jsx,ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2024,
-    sourceType: 'module',
-    globals: {
-      ...globals.browser,
-      ...globals.es2021,
-      process: true,
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'build/**',
+      'coverage/**',
+      '*.config.js',
+      'public/**'
+    ]
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        process: true,
+        __dirname: true,
+        module: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-duplicate-imports': 'error',
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true
+      }],
+      'no-console': ['warn', { 
+        allow: ['warn', 'error', 'info'] 
+      }],
+      'no-duplicate-imports': 'error',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'brace-style': ['error', '1tbs'],
+      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+      'object-curly-spacing': ['error', 'always']
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
   }
-};
+];
